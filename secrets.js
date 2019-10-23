ed492ac4e21565c246a6c9251119b98575c99830
Ben Clark <hello@clarkben.com>
2019-10-23 11:49:51 +0100

    Delete secrets.js

--- secrets.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  connection: {
-    host: '91.208.99.2',
-    port: '1214',
-    user: 'clarkben_ben',
-    password: 'buffalo',
-    database: 'clarkben_plant_manager'
-  }
-};
