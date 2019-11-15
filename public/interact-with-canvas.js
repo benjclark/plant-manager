f614068e260b59e6682c209add860f3871530984
Ben Clark <hello@clarkben.com>
2019-11-15 11:53:18 +0000

    doesn't drag beds if plant is being dragged and adds more icons

--- public/interact-with-canvas.js
+++ public/interact-with-canvas.js
@@ -157,7 +157,6 @@ function mouseUpHandler(e) {
     e.preventDefault();
     e.stopPropagation();
 
-    // clear all the dragging flags
     mouseEventVars.somethingIsBeingDragged = false;
 
     for (let z = 0; z < pm.beds.length; z++) {
@@ -168,43 +167,34 @@ function mouseUpHandler(e) {
     }
 }
 
+function dragIfClicked(rects, pm, mx, my, mouseEventVars) {
+    for (let i = (rects.length - 1); i >= 0; i--) {
+        const rect = rects[i];
+        if (mx > parseInt(rect.x, 10) && mx < parseInt(rect.x, 10) + parseInt(rect.width, 10) && my > parseInt(rect.y, 10) && my < parseInt(rect.y, 10) + parseInt(rect.height, 10)) {
+            mouseEventVars.somethingIsBeingDragged = true;
+            mouseEventVars.currentRectSelected = rect;
+            rect.isDragging = true;
+            pm.renderEditFormForCurrentlySelectedBed();
+            break;
+        }
+    }
+}
+
 function mouseDownHandler(e) {
     const pm = window.plantManager;
     const mouseEventVars = pm.mouseEventVariables;
     e.preventDefault();
     e.stopPropagation();
 
-    // get the current mouse position
     const mx = parseInt(e.clientX - pm.canvasOffsetX);
     const my = parseInt(e.clientY - pm.canvasOffsetY);
 
-    // clear all the dragging flags
     mouseEventVars.somethingIsBeingDragged = false;
 
-    // test each rect to see if mouse is inside
-    for (let i = (pm.beds.length - 1); i >= 0; i--) {
-        const b = pm.beds[i];
-        if (mx > parseInt(b.x, 10) && mx < parseInt(b.x, 10) + parseInt(b.width, 10) && my > parseInt(b.y, 10) && my < parseInt(b.y, 10) + parseInt(b.height, 10)) {
-            // if yes, set that rects isDragging=true
-            mouseEventVars.somethingIsBeingDragged = true;
-            mouseEventVars.currentRectSelected = b;
-            b.isDragging = true;
-            pm.renderEditFormForCurrentlySelectedBed();
-            break;
-        }
+    dragIfClicked(pm.plants, pm, mx, my, mouseEventVars);
+    if (mouseEventVars.somethingIsBeingDragged === false) {
+        dragIfClicked(pm.beds, pm, mx, my, mouseEventVars);
     }
-    for (let i = (pm.plants.length - 1); i >= 0; i--) {
-        const p = pm.plants[i];
-        if (mx > parseInt(p.x, 10) && mx < parseInt(p.x, 10) + parseInt(p.width, 10) && my > parseInt(p.y, 10) && my < parseInt(p.y, 10) + parseInt(p.height, 10)) {
-            // if yes, set that rects isDragging=true
-            mouseEventVars.somethingIsBeingDragged = true;
-            mouseEventVars.currentRectSelected = p;
-            p.isDragging = true;
-            pm.renderEditFormForCurrentlySelectedPlant();
-            break;
-        }
-    }
-    // save the current mouse position
     mouseEventVars.startX = mx;
     mouseEventVars.startY = my;
 }
@@ -212,23 +202,16 @@ function mouseDownHandler(e) {
 function mouseMoveHandler(e) {
     const pm = window.plantManager;
     const mouseEventVars = pm.mouseEventVariables;
-    // if we're dragging anything...
     if (mouseEventVars.somethingIsBeingDragged) {
         e.preventDefault();
         e.stopPropagation();
 
-        // get the current mouse position
         const mx = parseInt(e.clientX - pm.canvasOffsetX);
         const my = parseInt(e.clientY - pm.canvasOffsetY);
 
-        // calculate the distance the mouse has moved
-        // since the last mousemove
         const dx = mx - mouseEventVars.startX;
         const dy = my - mouseEventVars.startY;
 
-        // move each rect that isDragging
-        // by the distance the mouse has moved
-        // since the last mousemove
         for (let i = 0; i < pm.beds.length; i++) {
             const b = pm.beds[i];
             if (b.isDragging) {
@@ -245,10 +228,8 @@ function mouseMoveHandler(e) {
             }
         }
 
-        // redraw the scene with the new rect positions
         drawOnCanvas.drawAll();
 
-        // reset the starting mouse position for the next mousemove
         mouseEventVars.startX = mx;
         mouseEventVars.startY = my;
     }
